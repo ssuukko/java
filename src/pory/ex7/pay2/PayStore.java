215df7a1cd81870e2bb59fda665f08d11e158875
ssuukko <tlstpstpdl11@gmail.com>
2024-06-21 00:06:26 +0900

    다형성과 설계 정리 보충

--- src/pory/ex7/pay2/PayStore.java
+++ src/pory/ex7/pay2/PayStore.java
@@ -2,7 +2,7 @@ package pory.ex7.pay2;
 
 public abstract class PayStore {
 
-    public  static Pay findPay(String option) {
+    public static Pay findPay(String option) {
         if (option.equals("kakao")) {
             return new KakaoPay();
         } else if (option.equals("naver")) {
